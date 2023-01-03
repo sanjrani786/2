25806b83b40977a2c03f46fae92923e05d81ccb2
sanjrani786 <121862847+sanjrani786@users.noreply.github.com>
2023-01-03 17:28:42 +0500

    Delete ngrok.sh

--- ngrok.sh
+++
@@ -1,9 +0,0 @@
-echo "======================="
-echo "Download ngrok"
-echo "======================="
-rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
-chmod +x ng.sh
-./ng.sh
-clear
-
