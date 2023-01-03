acebab8f654ce45cbdf849902c6252274e575086
sanjrani786 <121862847+sanjrani786@users.noreply.github.com>
2023-01-03 17:20:00 +0500

    Update Lxde.sh

--- Lxde.sh
+++ Lxde.sh
@@ -4,17 +4,7 @@ chmod +x ng.sh
 ./ng.sh
 clear
 echo "======================="
-echo choose ngrok region
-echo "======================="
-echo "us - United States (Ohio)"
-echo "eu - Europe (Frankfurt)"
-echo "ap - Asia/Pacific (Singapore)"
-echo "au - Australia (Sydney)"
-echo "sa - South America (Sao Paulo)"
-echo "jp - Japan (Tokyo)"
-echo "in - India (Mumbai)"
-read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 3389 &>/dev/null &
+
 clear
 echo "================================"
 echo "Wait Downloading file 3-4 minutes"
