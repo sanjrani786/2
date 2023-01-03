12dab71b046e8bf79c034a3de74df81aa3b4fb2b
sanjrani786 <121862847+sanjrani786@users.noreply.github.com>
2023-01-03 17:22:02 +0500

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -6,16 +6,4 @@ wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 clear
-echo "======================="
-echo choose ngrok region
-echo "======================="
-echo "us - United States (Ohio)"
-echo "eu - Europe (Frankfurt)"
-echo "ap - Asia/Pacific (Singapore)"
-echo "au - Australia (Sydney)"
-echo "sa - South America (Sao Paulo)"
-echo "jp - Japan (Tokyo)"
-echo "in - India (Mumbai)"
-read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 3389 &>/dev/null &
-echo "======================="
+
