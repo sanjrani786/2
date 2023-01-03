6e595b2045cf2b9bc036e930e53e2905e75d43e4
sanjrani786 <121862847+sanjrani786@users.noreply.github.com>
2023-01-03 20:07:37 +0500

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,5 @@
 rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/ngrok3389 > /dev/null 2>&1
+wget -O ng.sh https://raw.githubusercontent.com/sanjrani786/2/main/2.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 clear
